7d25eaed757a08df653acfd9395653fb08b1558d
evarrr <eva.roussel@ensta.fr>
2023-04-24 20:41:36 +0200

    modification action

--- src/action.c
+++ src/action.c
@@ -2,5 +2,21 @@
 #include<stdlib.h>
 #include<string.h>
 #include "typedef.h"
-#include"case.h"
 
+
+void deplacement(case_t* case,case_t* map,joueur_t* joueur1,joueur_t* joueur2){
+    
+}
+
+void recolte_bonus(case_t* case,case_t* map,joueur_t* joueur1,joueur_t* joueur2){
+
+}
+
+void pose_bombe(case_t* case,case_t* map,joueur_t* joueur1,joueur_t* joueur2){
+
+}
+
+
+void action(case_t* case,case_t* map,joueur_t* joueur1,joueur_t* joueur2){
+    /**/
+}
