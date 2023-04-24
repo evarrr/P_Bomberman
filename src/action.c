4d416369511bac48a27e0b266cd04bc1a7a350f0
eva <eva.roussel@ensta.fr>
2023-04-24 22:08:11 +0200

    declaration de la fonction deplacement

--- src/action.c
+++ src/action.c
@@ -4,19 +4,18 @@
 #include "typedef.h"
 
 
-void deplacement(case_t* case,case_t* map,joueur_t* joueur1,joueur_t* joueur2){
+void deplacement(case_t* case,case_t* map,joueur_t* joueur,int direction){
     
-}
+    switch (direction){
+        case HAUT:
+            joueur->posy=joueur->posy-1;
+        
+        case BAS:
+            joueur->posy=joueur->posy +1;
 
-void recolte_bonus(case_t* case,case_t* map,joueur_t* joueur1,joueur_t* joueur2){
-
-}
-
-void pose_bombe(case_t* case,case_t* map,joueur_t* joueur1,joueur_t* joueur2){
-
-}
-
-
-void action(case_t* case,case_t* map,joueur_t* joueur1,joueur_t* joueur2){
-    /**/
+        case GAUCHE:
+            joueur->posx=joueur->posx -1;
+        case DROITE:
+            joueur ->posx=joueur->posx +1;
+    }
 }
